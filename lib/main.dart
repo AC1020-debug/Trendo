f2f7c535b5125ddae1e87382f078fdac3e3f46b3
ElaineCHL <elainechunghuilin@gmail.com>
2025-10-02 07:45:12 +0800

    Fix: Remove authentication feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:amplify_storage_s3/amplify_storage_s3.dart';
-import 'amplifyconfig.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'homepage.dart';
-import 'auth/auth_wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  if (!Amplify.isConfigured) {
-    final auth = AmplifyAuthCognito();
-    final storage = AmplifyStorageS3(); 
-
-    await Amplify.addPlugin(auth);
-    await Amplify.addPlugin(storage);
-
-    try {
-      await Amplify.configure(amplifyconfig);
-      print("Amplify configured successfully");
-    } catch (e) {
-      print("Error configuring Amplify: $e");
-    }
-  }
-
+  await dotenv.load(fileName: ".env");
   runApp(const TrendoApp());
 }
 
@@ -38,7 +19,7 @@ class TrendoApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const AuthWrapper(),
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
